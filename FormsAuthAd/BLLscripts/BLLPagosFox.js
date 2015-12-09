d9cc861bf9c522858a8ea126286b5cc944da250a
JVPRESTIGE <JORGEVIDAL199548@GMAIL.COM>
2015-12-09 17:57:18 -0500

    Compromisos

--- FormsAuthAd/BLLscripts/BLLPagosFox.js
+++ FormsAuthAd/BLLscripts/BLLPagosFox.js
@@ -23,6 +23,60 @@ BLLPagosFox.prototype.PagosFox = function (negocio) {
     })
 }
 
+
+BLLPagosFox.prototype.PagosFoxCompromisos = function (negocio) {
+    jsonData = "{'n':" + JSON.stringify(negocio) + "}";
+    $.ajax({
+        type: "POST", url: WAPagosNegocio, data: jsonData,
+        contentType: "application/json; charset=utf-8",
+        dataType: 'json',
+        async: true,
+        success: function (result) {
+            if (result.d === null) {
+
+            }
+            else {
+                BLLPagosFox.TablaPagosCompromisos(result.d);
+            }
+
+        },
+        error: function (error) { alert(error.responseText); }
+    })
+}
+
+
+
+
+
+BLLPagosFox.TablaPagosCompromisos = function (acuerdos) {
+    document.getElementById('pagosFox').innerHTML = "";
+    var tabla = '<table id="pagos" class="table table-striped table-bordered table-hover">';
+    tabla += "<thead>";
+    tabla += "<tr>";
+    tabla += "<th>Recibo No.</th>";;
+    tabla += "<th>Fecha de recibo</th>";
+    tabla += "<th>Vlr Recibo</th>";
+    tabla += "</tr>";
+    tabla += "</thead>";
+    tabla += "<tbody>";
+    $.each(acuerdos, function (i, item) {
+
+        tabla += " <tr class='CargarPagos' id=" + item.REFERENCIA1 + ">";
+        tabla += "<td>" + item.Nrecibo + "</td>";
+        tabla += "<td>" + item.Fecharecibo + "</td>";
+        tabla += "<td>" + utl.FormatNumero(item.Vlrrecibo) + "</td>";
+        tabla += "</tr>";
+
+    });
+    tabla += "</tbody>";
+    tabla += '</table>';
+    $('#pagosFox').append(tabla);
+   // $('#pagos').dataTable();
+
+}
+
+
+
 BLLPagosFox.TablaPagosNegocio = function (acuerdos) {
     document.getElementById('pagosFox').innerHTML = "";
     var tabla = '<table id="pagos" class="table table-striped table-bordered table-hover">';
---
+++ FormsAuthAd/Cartera/js/Compromisos.js
@@ -0,0 +1,70 @@
+﻿var utl = new BLLUtilidades();
+var Ac = new BLLAcuerdosFox();
+var Pag = new BLLPagosFox();
+var _admnego = (function () {
+    var WNegoID = "/Servicios/WNegocioFox.asmx/lisNegoID";
+    var negocio = utl.getUrl('negocio');
+
+    var _addHandlers = function () {
+
+    }
+    var getHoha = function (n) {
+        jsondata = "{'c':" + JSON.stringify(n) + "}"
+        $.ajax({
+            type: "POST", url: WNegoID, data: jsondata,
+            contentType: "application/json; charset=utf-8",
+            dataType: 'json',
+            async: true,
+            success: function (result) {
+                if (result.d == null) {
+
+                }
+                else {
+                    var nego = result.d;
+
+                    $('#TxtVlrInmueble').append(utl.FormatNumero(nego[0].VLRNEGOCIO));
+                    $("#TxtInmueble").append(nego[0].CODIGOINMUEBLE);
+                    $("#TxtCedula").append(nego[0].CEDULA_P);
+                    $("#TxtNombrecliente").append(nego[0].NOMBRECLIENTE);
+                    javascript: window.print();
+                }
+            },
+            error: function (obj, error, objError) { alert(obj.responseText); }
+        });
+    }
+
+    var imprimir = function () {
+
+    }
+
+    var _Inicio = function () {
+        /* getHoha(idhoja);
+         acuerdo(idhoja);
+         ConfiguracionNegocio();*/
+        getHoha(negocio);
+        imprimir();
+
+
+
+
+
+        Ac.CompromisosReporte(negocio);
+        Pag.PagosFoxCompromisos(negocio);
+    }
+
+    return {
+        init: function () {
+
+            _Inicio();
+            _addHandlers();
+
+        },
+    }
+
+}());
+
+$(document).ready(function () {
+
+    _admnego.init();
+
+})
