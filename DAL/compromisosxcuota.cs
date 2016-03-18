8bb3d54bf6df666ffb2de99a0ee5105c61bd5449
JVPRESTIGE <JORGEVIDAL199548@GMAIL.COM>
2016-03-18 17:32:03 -0500

    Añadir la tabla

--- DAL/compromisosxcuota.cs
+++ DAL/compromisosxcuota.cs
@@ -12,19 +12,10 @@ namespace DAL
     using System;
     using System.Collections.Generic;
     
-    public partial class empresas
+    public partial class compromisosxcuota
     {
-        public empresas()
-        {
-            this.clientes = new HashSet<clientes>();
-            this.clientes1 = new HashSet<clientes>();
-        }
-    
-        public int ID_EMP { get; set; }
-        public string NOMBRE_EMP { get; set; }
-        public string TEL_EMP { get; set; }
-    
-        public virtual ICollection<clientes> clientes { get; set; }
-        public virtual ICollection<clientes> clientes1 { get; set; }
+        public int ID { get; set; }
+        public Nullable<int> ID_TAREA { get; set; }
+        public string CODIGO { get; set; }
     }
 }
