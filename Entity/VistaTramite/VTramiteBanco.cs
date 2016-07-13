bc17e7362137a6ea553861556d4f6859219817f9
JVPRESTIGE <JORGEVIDAL199548@GMAIL.COM>
2016-07-13 16:34:07 -0500

    asvas

---
+++ Entity/VistaTramite/VTramiteBanco.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.VistaTramite
+{
+    public class VTramiteBanco
+    {
+        public int id { get; set; }
+        public string Nombre { get; set; }
+        public string Banco { get; set; }
+        public int idbanco { get; set; }
+
+
+    }
+}
