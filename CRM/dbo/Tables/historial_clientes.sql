7fdc50516ed89d70192ea2af32b64d4da5daf627
JVPRESTIGE <JORGEVIDAL1995@HOTMAIL.COM>
2016-09-17 12:00:47 -0500

    entregas

--- CRM/dbo/Tables/historial_clientes.sql
+++ CRM/dbo/Tables/historial_clientes.sql
@@ -5,11 +5,11 @@
     [DESCRIPCIONH] VARCHAR (90) NULL,
     [FECHA]        DATE         NULL,
     CONSTRAINT [PK__historia__76E6C502B92FC233] PRIMARY KEY CLUSTERED ([ID_HISTORIAL] ASC),
-    CONSTRAINT [FK__historial__clien__1F04813B] FOREIGN KEY ([CLIENTEH]) REFERENCES [dbo].[clientes] ([CEDULA]),
-    CONSTRAINT [FK__historial__clien__4F47C5E3] FOREIGN KEY ([CLIENTEH]) REFERENCES [dbo].[clientes] ([CEDULA]),
     CONSTRAINT [FK__historial__traba__1FF8A574] FOREIGN KEY ([TRABAJADOR]) REFERENCES [dbo].[trabajadores] ([T_CEDULA]),
     CONSTRAINT [FK__historial__traba__503BEA1C] FOREIGN KEY ([TRABAJADOR]) REFERENCES [dbo].[trabajadores] ([T_CEDULA])
 );
+
+
 
 
 
