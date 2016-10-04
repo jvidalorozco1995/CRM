352e3636468c6d719ce88cf7c6bd79dae14f7e06
Carparthus <carparthus@gmail.com>
2016-10-04 09:05:33 -0500

    servicios

--- BLLCRM/BLLAmbienteXitems.cs
+++ BLLCRM/BLLAmbienteXitems.cs
@@ -49,5 +49,112 @@ namespace BLLCRM
                 return 0;
             }
         }
+        public int UpdatePosicionItemXambiente(ItemXambiente i)
+        {
+
+            try
+            {
+
+                int? posicion = 1;
+                int? actual = 1;
+
+                var range1 = bd.ItemXambiente
+               .Where(t => t.Id == i.Id).FirstOrDefault();
+                posicion = i.Consecutivo;
+                actual = range1.Consecutivo;
+                //if (range1 != null)
+                //{
+                //    range1.Posicion = i.Posicion;
+                //    bd.SaveChanges();
+                //}
+                if (actual < posicion)
+                {
+                    var range = bd.ItemXambiente.Where(t => t.IdAmbiente == i.IdAmbiente && t.Consecutivo >= actual && t.Consecutivo <= posicion).ToList();
+                    foreach (var item in range)
+                    {
+                        if (item.Consecutivo == actual)
+                        {
+                            item.Consecutivo = posicion;
+                            bd.SaveChanges();
+                        }
+                        else if (item.Consecutivo == posicion)
+                        {
+                            item.Consecutivo = posicion - 1;
+                            bd.SaveChanges();
+                        }
+                        else
+                        {
+                            item.Consecutivo = item.Consecutivo - 1;
+                            bd.SaveChanges();
+                        }
+
+
+                    }
+                }
+                else
+                {
+                    var range = bd.ItemXambiente.Where(t => t.IdAmbiente == i.IdAmbiente && t.Consecutivo <= actual && t.Consecutivo >= posicion).ToList();
+                    foreach (var item in range)
+                    {
+                        if (item.Consecutivo == actual)
+                        {
+                            item.Consecutivo = posicion;
+                            bd.SaveChanges();
+                        }
+                        else if (item.Consecutivo == posicion)
+                        {
+                            item.Consecutivo = posicion + 1;
+                            bd.SaveChanges();
+                        }
+                        else
+                        {
+                            item.Consecutivo = item.Consecutivo + 1;
+                            bd.SaveChanges();
+                        }
+
+
+                    }
+                }
+
+
+                return 1;
+            }
+            catch (Exception ex)
+            {
+                return 0;
+                throw;
+            }
+        }
+        public List<ItemXambiente> ListItemXambiente(int id)
+        {
+
+            try
+            {
+                List<ItemXambiente> lisb = bd.ItemXambiente.OrderBy(t => t.Consecutivo).Where(t => t.IdAmbiente == id).ToList();
+                //bd.compromisosxcuota.ToList();
+                List<ItemXambiente> lisbcrm = new List<ItemXambiente>();
+                if (lisb.Count.Equals(0))
+                {
+                    return lisbcrm;
+                }
+                else
+                {
+                    foreach (var item in lisb)
+                    {
+                        ItemXambiente entb = new ItemXambiente();
+                        entb.Id = item.Id;
+                        entb.IdAmbiente = item.IdAmbiente;
+                        entb.IdItem = item.IdItem;
+                        lisbcrm.Add(entb);
+                    }
+                    return lisbcrm;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
